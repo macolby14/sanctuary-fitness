70d67b6403272953f50d5892b515063aef93ec79
Mark Colby <macolby14@gmail.com>
2020-10-19 21:07:08 -1000

    add Mobile Menu

--- components/my-app-bar.tsx
+++ components/my-app-bar.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import {
-  AppBar, Container, Typography, Button,
+  AppBar, Container, Typography, Button, Menu, MenuItem, IconButton,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -24,9 +25,6 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  navButton: {
-    // color: theme.palette.primary.contrastText,
-  },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
@@ -46,21 +44,63 @@ const useStyles = makeStyles((theme) => ({
 export default function MyAppBar() {
   const classes = useStyles();
 
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <p>Try It Now</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Classes</p>
+      </MenuItem>
+      <MenuItem>
+        <p>About Me</p>
+      </MenuItem>
+      <MenuItem>
+        <p>Contact Us</p>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
-    <AppBar className={classes.root} position="sticky">
-      <Container className={classes.container}>
-        <img className={classes.img} src="static/images/inner_sanct_logo_50px.png" alt="Inner Sanctuary" />
-        <Typography className={classes.title} variant="h6">Inner Sanctuary Fitness</Typography>
-        <div className={classes.sectionDesktop}>
-          <Button color="inherit" component={Link} href="/">Try It Now</Button>
-          <Button color="inherit" component={Link} href="/">Classes</Button>
-          <Button color="inherit" component={Link} href="/">About Me</Button>
-          <Button color="inherit" component={Link} href="/">Contact Us</Button>
-        </div>
-        <div className={classes.sectionMobile}>
-          <MenuIcon color="inherit" />
-        </div>
-      </Container>
-    </AppBar>
+    <>
+      <AppBar className={classes.root} position="sticky">
+        <Container className={classes.container}>
+          <img className={classes.img} src="static/images/inner_sanct_logo_50px.png" alt="Inner Sanctuary" />
+          <Typography className={classes.title} variant="h6">Inner Sanctuary Fitness</Typography>
+          <div className={classes.sectionDesktop}>
+            <Button color="inherit" component={Link} href="/">Try It Now</Button>
+            <Button color="inherit" component={Link} href="/">Classes</Button>
+            <Button color="inherit" component={Link} href="/">About Me</Button>
+            <Button color="inherit" component={Link} href="/">Contact Us</Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton color="inherit" onClick={handleMobileMenuOpen}>
+              <MenuIcon />
+            </IconButton>
+          </div>
+        </Container>
+      </AppBar>
+      {renderMobileMenu}
+    </>
   );
 }
