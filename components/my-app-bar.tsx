0467b831e035337e75f5a0f6fa4713cd7a576541
Mark Colby <macolby14@gmail.com>
2020-10-19 20:30:00 -1000

    add basic buttons to navbar and theme colors

--- components/my-app-bar.tsx
+++ components/my-app-bar.tsx
@@ -1,7 +1,11 @@
-import { AppBar, Container, Typography } from '@material-ui/core';
+import {
+  AppBar, Container, Typography, Button,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+import Link from './link';
+
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -11,13 +15,16 @@ const useStyles = makeStyles(() => ({
     alignContent: 'center',
   },
   img: {
-    padding: '0 20px',
+    padding: '0 20px 0 0',
   },
   title: {
     flexGrow: 1,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  navButton: {
+    color: theme.palette.primary.contrastText,
   },
 }));
 
@@ -26,9 +33,13 @@ export default function MyAppBar() {
 
   return (
     <AppBar className={classes.root} position="sticky">
-      <Container className={classes.container}>
+      <Container disableGutters className={classes.container}>
         <img className={classes.img} src="static/images/inner_sanct_logo_50px.png" alt="Inner Sanctuary" />
         <Typography className={classes.title} variant="h6">Inner Sanctuary Fitness</Typography>
+        <Button className={classes.navButton} component={Link} href="/">Try It Now</Button>
+        <Button className={classes.navButton} href="/">Classes</Button>
+        <Button className={classes.navButton}>About Me</Button>
+        <Button className={classes.navButton}>Contact Us</Button>
       </Container>
     </AppBar>
   );
--- styles/theme.js
+++ styles/theme.js
@@ -1,6 +1,17 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4854D4',
+      contrastText: 'white',
+    },
+    secondary: {
+      main: '#489ED4',
+    },
+
+  },
+});
 const responsiveTheme = responsiveFontSizes(theme);
 
 export default responsiveTheme;
