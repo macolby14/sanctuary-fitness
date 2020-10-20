a193755e491aa0afedf0e611af3b9add87fd680f
Mark Colby <macolby14@gmail.com>
2020-10-19 16:18:29 -1000

    add image to MyAppBar

--- components/my-app-bar.tsx
+++ components/my-app-bar.tsx
@@ -1,10 +1,34 @@
-import { AppBar, Container } from '@material-ui/core';
+import { AppBar, Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    flexGrow: 1,
+    display: 'flex',
+    alignContent: 'center',
+  },
+  img: {
+    padding: '0 20px',
+  },
+  title: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+}));
 
 export default function MyAppBar() {
+  const classes = useStyles();
+
   return (
-    <AppBar>
-      <Container>
-        <h6>Menu</h6>
+    <AppBar className={classes.root} position="sticky">
+      <Container className={classes.container}>
+        <img className={classes.img} src="static/images/inner_sanct_logo_50px.png" alt="Inner Sanctuary" />
+        <Typography className={classes.title} variant="h6">Inner Sanctuary Fitness</Typography>
       </Container>
     </AppBar>
   );
