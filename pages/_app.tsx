fb9823d93988039850045155e6916f7726046810
Mark Colby <macolby14@gmail.com>
2020-10-19 16:04:45 -1000

    changes for nextjs and mui compatability errors for SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,40 @@
 // Disable eslint rule for HOC
 /* eslint-disable react/jsx-props-no-spreading */
-
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Container, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import MyAppBar from '../components/my-app-bar';
+import theme from '../styles/theme.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
-      <CssBaseline />
       <Head>
         <title>Inner Sanctuary Fitness</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <MyAppBar />
-      <main>
-        <Container maxWidth="lg" disableGutters>
-          <Component {...pageProps} />
-        </Container>
-      </main>
-      <footer><h2>Footer goes here</h2></footer>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyAppBar />
+        <main>
+          <Container maxWidth="lg" disableGutters>
+            <Component {...pageProps} />
+          </Container>
+        </main>
+        <footer><h2>Footer goes here</h2></footer>
+      </ThemeProvider>
     </>
   );
 }
