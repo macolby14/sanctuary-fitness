5ffd380c4a77a76bed8b6f2b05c1ddefbaa26b68
Mark Colby <macolby14@gmail.com>
2020-10-27 21:09:07 -1000

    add MoreInfo comp - finish index.tsx

--- components/hero-box.tsx
+++ components/hero-box.tsx
@@ -1,18 +1,12 @@
 import { Box, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import Image from 'next/image';
 
-const useStyles = makeStyles(() => ({
-}));
-
 export default function HeroBox() {
-  const classes = useStyles();
-
   return (
     <Box display="flex" flexWrap="wrap" justifyContent="center" alignItems="center">
       <Image src="/static/images/inner_sanct_logo_250px.png" alt="Inner Sanctuary" height={250} width={250} />
-      <Box display="flex" alignItems="center" flexDirection="column">
-        <Typography variant="h2">Inner Sancutary Fitness</Typography>
+      <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column">
+        <Typography variant="h2" align="center">Inner Sanctuary Fitness</Typography>
         <Typography variant="h6">
           Stretch and Strengthen Your Body
         </Typography>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import HeroBox from '../components/hero-box';
 import Link from '../components/link';
+import MoreInfo from '../components/more-info';
 
 const useStyles = makeStyles((theme) => ({
-  margin: {
+  buttonMargin: {
     margin: theme.spacing(2, 'auto'),
+  },
+  margin: {
+    margin: theme.spacing(1, 2),
   },
 }));
 
@@ -22,32 +26,12 @@ export default function Home() {
         <HeroBox />
         <Box display="flex" flexDirection="column" alignItems="center" m={2}>
           { /* TODO - Fix variant type conflict between MUI Link and MUI Button */ }
-          <Button component={Link} className={classes.margin} href="/" color="primary" variant={'contained' as any}>Get Started</Button>
+          <Button component={Link} className={classes.buttonMargin} href="/" color="primary" variant={'contained' as any}>Get Started</Button>
           <Typography variant="h6">Classes for all levels in Oahu, Hawaii</Typography>
           <MoreInfo />
         </Box>
 
       </main>
     </div>
-  );
-}
-
-function MoreInfo() {
-  return (
-    <Box display="flex" flexDirection="column" alignItems="center" m={2}>
-      <InfoRow />
-      <InfoRow />
-      <InfoRow />
-    </Box>
-  );
-}
-
-function InfoRow() {
-  return (
-    <Box display="flex" m={2}>
-      <h6>Image</h6>
-      <h6>Body</h6>
-      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dignissimos culpa ab qui maxime magnam. Perspiciatis distinctio optio nemo repellendus esse dolore dignissimos similique debitis, nisi error reiciendis excepturi aspernatur?</Typography>
-    </Box>
   );
 }
