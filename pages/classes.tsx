da49a900468b4645f6012b58c607bbeae33ac6bc
Mark Colby <macolby14@gmail.com>
2020-10-28 19:48:42 -1000

    add basic date/time picker to classes

--- pages/classes.tsx
+++ pages/classes.tsx
@@ -1,3 +1,20 @@
+import React, { useState } from 'react';
+import DateFnsUtils from '@date-io/date-fns'; // choose your lib
+import {
+  DateTimePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+
 export default function Classes() {
-  return <h1>Classes Page</h1>;
+  const [selectedDate, handleDateChange] = useState(new Date());
+
+  return (
+    <>
+      <h1>Classes Page</h1>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        {/* Any from example https://material-ui-pickers.dev/getting-started/usage */}
+        <DateTimePicker value={selectedDate} onChange={handleDateChange as any} />
+      </MuiPickersUtilsProvider>
+    </>
+  );
 }
