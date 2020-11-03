ba53feb2f77e3c5f1c7e5cacb06f9b47157266e9
Mark Colby <macolby14@gmail.com>
2020-11-03 00:56:18 -1000

    eslint format on save with settings.json

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
+
+// Serve static files from public
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
