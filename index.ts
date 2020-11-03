76c5c59ef8c3369593124212745c53a0b7cff29e
Mark Colby <macolby14@gmail.com>
2020-11-03 00:59:55 -1000

    setup for static serving

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import path from 'path';
 // rest of the code remains same
 const app = express();
+
 const PORT = 8000;
 
-// Serve static files from public
-app.use(express.static('public'));
+// Serve static files from public -> served under static url
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.get('/test', (req, res) => res.send('Test'));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
