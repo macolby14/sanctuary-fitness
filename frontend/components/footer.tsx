d63f06912dbb017367616c47dfdee491ab0b1786
Mark Colby <macolby14@gmail.com>
2020-11-23 20:04:39 -1000

    add snapshot test for footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Container, Typography, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import InstagramIcon from '@material-ui/icons/Instagram';
