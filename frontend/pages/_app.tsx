b98fc144934e549c84d330745d17487f6269b622
Mark Colby <macolby14@gmail.com>
2020-11-25 12:31:34 -1000

    add meta tags to app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -25,6 +25,24 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Inner Sanctuary Fitness</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Inner Sanctuary Fitness is a stretching and 
+        strengthening class that features Christian prayer and meditation in the Catholic Tradition. It is part of the Pietra Fitness program." />
+        <meta name="keywords" content="Pietra Fitness, Chritian, Catholic, Yoga, Spiritual, Fitness, Strengthening" />
+        <meta name="author" content="Mark Colby, Kimberly Colby" />
+        <meta property="og:local" content="en_US"/>
+        <meta property="og:title" content="Inner Sanctuary Fitness"/>
+        <meta property="og:type" content="Website"/>
+        <meta property="og:url" content="https://www.innersanctuaryfitness.com"/>
+        <meta property="og:site_name" content="Inner Sanctuary Fitness"/>
+        <meta property="og:image:secure_url" content="https://innersanctuaryfitness.com/static/images/gallery1_6240_4160_gimp_tiny.jpg"/>
+        <meta property="og:image:width" content="6240"/>
+        <meta property="og:image:height" content="4160"/>
+        <meta property="og:image:alt" content="A picture of Kimberly Colby stretching on the beach doing Pietra Fitness"/>
+        <meta property="og:description"  content="Inner Sanctuary Fitness is a stretching and 
+        strengthening class that features Christian prayer and meditation in the Catholic Tradition. It is part of the Pietra Fitness program." /> 
+        <meta property="twitter:card" content="sumamry" />
+        <meta property="twitter:creator" content="@macolby14" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
