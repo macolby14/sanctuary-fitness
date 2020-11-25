a365126c75ee955cb249535ae7328afec3480b15
Mark Colby <macolby14@gmail.com>
2020-11-25 11:45:25 -1000

    add Contact Me page

--- frontend/pages/contact.tsx
+++ frontend/pages/contact.tsx
@@ -1,3 +1,15 @@
+import {Typography, Link} from '@material-ui/core';
+
+
 export default function Contact() {
-  return <h1>Contact Page</h1>;
+  return <>
+   <Typography variant="h1" align="center">Contact Me</Typography>
+   <br/>
+   <Typography variant="h4" align="center">Questions about scheduling a class?</Typography>
+   <Typography variant="h4" align="center">Want to learn more?</Typography>
+   <br/>
+   <Typography variant="h4" align="center">Email me at <Link href="mailto:innersanctuaryfitness@gmail.com" target="_blank" rel="noopener noreferrer">innersanctuaryfitness@gmail.com</Link></Typography>
+   <br/>
+   <Typography variant="h3" align="center">Thank you!</Typography>
+  </>;
 }
