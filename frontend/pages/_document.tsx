5b38995e330727e38a344875f70b576bceea31b7
Mark Colby <macolby14@gmail.com>
2020-11-26 21:13:39 -1000

    simplify google fonts download

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
